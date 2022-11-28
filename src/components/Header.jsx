6cd897335899ee8900425bead6be816b42e24912
Alek Sung <aleksungis@gmail.com>
2022-11-27 19:09:31 -0500

    Created info bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,94 @@
+import gmailIcon from "../assets/gmail_icon.jpg";
+import linkedinIcon from "../assets/linkedin_icon.png"
+import phoneIcon from "../assets/phone_icon.png";
+
+const PHONE_ID = "phone-number";
+const EMAIL_ID = "email-address";
+
+function Name() {
+  return (
+    <div id='header-name'>
+      Alexander F. Sung
+    </div>
+  );
+}
+
+function InfoBar() {
+  function editPhoneNumber(e) {
+    e.preventDefault();
+    var phoneNumber = prompt("Enter phone number:");
+    if (phoneNumber) {
+      document.getElementById(PHONE_ID).textContent = phoneNumber;
+    }
+  }
+
+  function editEmailAddress(e) {
+    e.preventDefault();
+    var emailAddress = prompt("Enter email address:");
+    if (emailAddress) {
+      document.getElementById(EMAIL_ID).textContent = emailAddress;
+    }
+  }
+
+  return (
+    <div id='info-bar'>
+      <span>
+        <img
+          className="info-icon"
+          onClick={editPhoneNumber}
+          src={phoneIcon}
+          alt="phone icon"
+        />
+        <span>&nbsp;</span>
+        <span id={PHONE_ID}>
+          xxx-xxx-xxxx
+        </span>
+      </span>
+
+      <span>&nbsp;|&nbsp;</span>
+
+      <span>
+        <img
+          className="info-icon"
+          onClick={editEmailAddress}
+          src={gmailIcon}
+          alt="Gmail icon"
+        />
+        <span>&nbsp;</span>
+        <span id={EMAIL_ID}>
+          xxxxxxxxxx@gmail.com
+        </span>
+      </span>
+
+      <span>&nbsp;|&nbsp;</span>
+
+      <span>
+        <a href="https://www.linkedin.com/in/sungalexander">
+          <img
+            className="info-icon"
+            src={linkedinIcon}
+            alt="Attribution: https://www.flaticon.com/free-icons/linkedin"
+            style={{width: "0.90em", height: "auto"}}
+          />
+        </a>
+        <span>&nbsp;</span>
+        <a href="https://www.linkedin.com/in/sungalexander"
+          className="no-style-link"
+        >
+          linkedin.com/in/sungalexander
+        </a>
+      </span>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className='header'>
+      <Name />
+      <InfoBar />
+    </div>
+  );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Resume from './components/Resume';
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Resume />
+    <>
+      <Resume />
+    </>
   </React.StrictMode>
 );
