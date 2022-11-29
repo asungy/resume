ebc6a9910c75602d741cf5e002269a823ae35299
Alek Sung <aleksungis@gmail.com>
2022-11-29 01:09:22 -0500

    Added employment header

--- src/components/Employment.jsx
+++ src/components/Employment.jsx
@@ -1,8 +1,46 @@
 function SectionHeader() {
   return (
     <div className="section-header">
-      <p>Employment</p>
+      <span>Employment</span>
       <hr/>
+    </div>
+  );
+}
+
+const entries = [
+  {
+    title: "Software Engineer",
+    company: "Epistemix (start-up)",
+    location: "Remote",
+    start: "02/2022",
+    end: "Current",
+    points: [
+      "TODO-1",
+      "TODO-2",
+      "TODO-3"
+    ]
+  }
+];
+
+function Entry(props) {
+  let entry = props.entry;
+  return (
+    <div className="employment-entry">
+      <div className="entry-header">
+        <span className="title">{entry.title}</span>
+        <span className="company">{entry.company}</span>
+        <div className="entry-header-right">
+          <span>{entry.start} - {entry.end}</span>
+          <span style={{fontStyle: "italic"}}>{entry.location}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Entries() {
+  return (
+    <div>
     </div>
   );
 }
@@ -11,6 +49,7 @@ function Employment() {
   return (
     <div id="employment">
       <SectionHeader />
+      <Entry key="Epistemix" entry={entries[0]} />
     </div>
   );
 }
