04146ad5d0dd4de12837f32486c743397a31985c
Alek Sung <aleksungis@gmail.com>
2022-11-30 00:13:41 -0500

    Finished education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,10 +9,39 @@ function SectionHeader() {
   );
 }
 
+const virginaTechEntry = {
+  school: "Virginia Tech",
+  degree: "Bachelor\'s of Science",
+  domain: "Computer Science",
+  location: "Blacksburg, VA",
+  start_year: "2013",
+  end_year: "2018",
+};
+
+function Entry(props) {
+  let entry = props.entry;
+  return (
+    <div className="entry">
+      <div className="left-section">
+        <span className="degree">{entry.degree}</span>
+        <span className="domain">{entry.domain}</span>
+      </div>
+
+      <span className="school">{entry.school}</span>
+
+      <div className="right-section">
+        <span className="year">{entry.start_year} - {entry.end_year}</span>
+        <span className="location">{entry.location}</span>
+      </div>
+    </div>
+  );
+}
+
 function Education() {
   return (
     <div id="education">
       <SectionHeader />
+      <Entry entry={virginaTechEntry} />
     </div>
   );
 }
