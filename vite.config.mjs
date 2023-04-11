22b5410f4ee407119976a28daa2ae71018409d7c
asungy <aleksungis@gmail.com>
2023-04-10 20:14:21 -0400

    Update vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'npm:vite@^3.2.3';
-import react from 'npm:@vitejs/plugin-react@^2.2.0';
+import { defineConfig } from 'npm:vite@^4.0.4';
+import react from 'npm:@vitejs/plugin-react@^3.0.1';
 
 import 'npm:react@^18.2.0';
 import 'npm:react-dom@^18.2.0/client';
@@ -7,4 +7,4 @@ import 'npm:react-dom@^18.2.0/client';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
-});
+})
